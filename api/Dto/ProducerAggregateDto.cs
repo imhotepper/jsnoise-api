32693be228258bee11cb10101ccb5357740a0490
dragosh <stefanescu.dragos@gmail.com>
2022-11-27 12:23:33 +0200

    removed old code netapp 2.2

--- api/Dto/ProducerAggregateDto.cs
+++
@@ -1,9 +0,0 @@
-namespace CoreJsNoise.Dto
-{
-    public class ProducerAggregateDto
-    {
-        public string Name { get; set; }
-        public  int Count { get; set; }
-        public int Id { get; set; }
-    }
-}
